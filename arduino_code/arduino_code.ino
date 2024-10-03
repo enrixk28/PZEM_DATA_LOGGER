5591ddbc34e2c7bf89ea07aba17d34e730d951c5
enrixk28 <johnenrickelias28@gmail.com>
2024-10-04 06:57:11 +0800

    added PZEM energy reset on setup

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -1,6 +1,7 @@
 #include <SoftwareSerial.h>
 #include <PZEM004Tv30.h>
 
+
 // Define SoftwareSerial objects for each PZEM
 SoftwareSerial pzemSerial1(2, 3);  // RX, TX for PZEM1
 SoftwareSerial pzemSerial2(4, 5);  // RX, TX for PZEM2
@@ -14,25 +15,26 @@ PZEM004Tv30 pzem2(pzemSerial2);
 PZEM004Tv30 pzem3(pzemSerial3);
 
 // Calibration factors
-float currentCalibrationFactor = 0.5;  // Calibration factor for current (Amperes)
-float voltageCalibrationFactor = 1.0;  // Calibration factor for voltage (Volts)
-float powerCalibrationFactor = 1.0;    // Calibration factor for power (Watts)
-float energyCalibrationFactor = 1.5;   // Calibration factor for energy (kWh)
-float pfCalibrationFactor = 1.0;     // Calibration factor for power factor (as a percentage)
+float currentCalibrationFactor[3] = {1.0, 1.0, 1.0};  // Calibration factor for current (Amperes)
+float voltageCalibrationFactor[3] = {1.0, 1.0, 1.0};  // Calibration factor for voltage (Volts)
+float powerCalibrationFactor[3] = {1.0, 1.0, 1.0};    // Calibration factor for power (Watts)
+float energyCalibrationFactor[3] = {1.0, 1.0, 1.0};   // Calibration factor for energy (kWh)
+float pfCalibrationFactor[3] = {1.0, 1.0, 1.0};     // Calibration factor for power factor (as a percentage)
 
-// Interval for reading values (1 hour in milliseconds)
-// const unsigned long interval = 360000;
-const unsigned long interval = 12000; // temporary interval ( 5 seconds )
+/*********************   1 HOUR INTERVAL   *********************/
+// const unsigned long interval = 360000; // Read Sensor Per Hour
+/*********************   12 SECOND INTERVAL   *********************/
+const unsigned long interval = 12000; // temporary interval ( 12 seconds )
 unsigned long previousMillis = 0;
 
 // Read values from PZEM004T
-void read_values(PZEM004Tv30 pzem, float results[6]) {
-    results[0] = pzem.voltage(); //* voltageCalibrationFactor;
-    results[1] = pzem.current(); //* currentCalibrationFactor;
-    results[2] = pzem.power(); //* powerCalibrationFactor;
-    results[3] = pzem.energy(); //* energyCalibrationFactor;
+void read_values(PZEM004Tv30 pzem, float results[6], int index) {
+    results[0] = pzem.voltage() * voltageCalibrationFactor[index];
+    results[1] = pzem.current() * currentCalibrationFactor[index];
+    results[2] = pzem.power() * powerCalibrationFactor[index];
+    results[3] = pzem.energy() * energyCalibrationFactor[index];
     results[4] = pzem.frequency();
-    results[5] = pzem.pf(); //* pfCalibrationFactor;
+    results[5] = pzem.pf() * pfCalibrationFactor[index];
 }
 
 // // Read values from PZEM004T
@@ -79,12 +81,17 @@ void setup() {
   pzemSerial2.begin(9600);
   pzemSerial3.begin(9600);
   arduinoESPSerial.begin(9600);
+  pzem1.resetEnergy();
+  pzem2.resetEnergy();
+  pzem3.resetEnergy();
 }
 
 void loop() {
   // Time when device starts running
   unsigned long currentMillis = millis();
+  /*********************   1 HOUR INTERVAL   *********************/
   // unsigned long hours = currentMillis / 1000 / 60 / 60; // 1 hour conversion
+  /*********************   12 SECOND INTERVAL   *********************/
   unsigned long hours = currentMillis / 1000; // 1 second conversion
 
   if (currentMillis - previousMillis >= interval) {
@@ -95,24 +102,21 @@ void loop() {
     // Read data from PZEM1
     Serial.println("Reading from PZEM1");
     float results1[6];
-    read_values(pzem1, results1);
+    read_values(pzem1, results1, 0);
     print_values(results1);
 
     // Read data from PZEM2
     Serial.println("Reading from PZEM2");
     float results2[6];
-    read_values(pzem2, results2);
+    read_values(pzem2, results2, 1);
     print_values(results2);
 
     // Read data from PZEM3
     Serial.println("Reading from PZEM3");
     float results3[6];
-    read_values(pzem3, results3);
+    read_values(pzem3, results3, 2);
     print_values(results3);
-
-  //  Send data to ESP32 using SoftwareSerial UART 
-  //  Serial.println("Sending Data Test");
-  //  arduinoESPSerial.write("Hello");
+    
     String hourString = String(hours);
     Serial.println("Duration: " + hourString + " h");
     arduinoESPSerial.print(hourString + ",");
